f867257c2a2970c3de17f1c6b45338836dbde8cc
WIL-TZY <willowitzy@gmail.com>
2024-06-10 11:11:05 -0300

    Modificando o HTML

--- javaweb/src/main/java/br/com/nael/javaweb/Application.java
+++ javaweb/src/main/java/br/com/nael/javaweb/Application.java
@@ -7,15 +7,28 @@ import java.io.File;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
+
+/*
+Buildar com o Maven:
+	mvn clean
+	mvn package
+Roda o programa:
+	java -jar target/javaweb-0.0.1-jar-with-dependencies.jar
+*/
 public class Application {
 
     public static void main(String[] args) throws LifecycleException {
     	Tomcat tomcat = new Tomcat();
-    	tomcat.setPort(8889);
+    	tomcat.setPort(8888);
     	tomcat.getConnector(); // precisa pra funcionar
         
+    	// Fazendo com o Tomcat (Webapp)
+    	/*
     	Context ctx = tomcat.addContext("", new File(".").getAbsolutePath());
     	// Argumentos são: context, string, service
     	Tomcat.addServlet(ctx, "HelloServlet", new HelloServlet());
@@ -23,6 +36,16 @@ public class Application {
     	
     	Tomcat.addServlet(ctx, "InfoServlet", new InfoServlet());
     	ctx.addServletMappingDecoded("/info", "InfoServlet");
+    	*/
+    	
+    	// Fazendo sem o Tomcat (Webapp)
+    	Context ctx = tomcat.addWebapp("/", new File("src/main/webapp/").getAbsolutePath());
+    	
+    	var novoDirClasses = new File("target/classes").getAbsolutePath();
+    	WebResourceRoot resources = new StandardRoot(ctx);
+    	var dirBaseClasses = new DirResourceSet(resources, "/WEB-INF/classes", novoDirClasses, "/");
+    	resources.addPreResources(dirBaseClasses);
+    	ctx.setResources(resources);
     	
     	tomcat.start();
     	tomcat.getServer().await();
--- javaweb/src/main/java/controller/HelloServlet.java
+++ javaweb/src/main/java/controller/HelloServlet.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,15 +11,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ola")
+/*
+A anotação @WebServlet é usada em classes 
+que estendem HttpServlet para configurar uma servlet 
+diretamente na classe, sem a necessidade de definir a 
+servlet no arquivo web.xml.
+*/
+
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		Path filePath = Paths.get(getServletContext().getRealPath("/hello.html"));
+		
+		if (Files.exists(filePath)) {
+			response.getWriter().write(Files.readString(filePath));
+		}
+		else {
+			response.getWriter().write("<h1>Arquivo não encontrado</h1>");
+			}
+		
+		/*
 		response.getWriter().write("<h1>Olá, mundo!</h1>");
 		response.getWriter().flush();
 		response.getWriter().close();
+		*/
 	}
 }
